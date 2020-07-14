a533ca5da96d681bc72b64581a95040fb90ad72e
yolandalopezfernandez <yolandaa020@outlook.com>
2020-07-14 18:16:43 -0300

    clase 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,10 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	let nombre; // declaro variable nombre
+
+	nombre = prompt("Ingrese su Nombre"); // igual primero ejecuta a la derecha y despues la izq
+
+	alert(nombre);
 }
 
